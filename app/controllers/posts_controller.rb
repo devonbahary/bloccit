95d4c1aa6dc495c874709d2b52ffccfaa555fee7
devonbahary <devonbahary@gmail.com>
2017-07-12 01:01:57 +0000

    18 Rails: Intro to CRUD | Assignment Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    
+    # censor every fifth post with title 'SPAM'
+    @posts.each_with_index { |post, index| post.title = 'SPAM' if index % 5 == 0 }
   end
 
   def show
