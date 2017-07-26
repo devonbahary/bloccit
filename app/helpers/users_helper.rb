2bd1899cd43cdfa50e82a0efec3e565bacda5d2f
devonbahary <devonbahary@gmail.com>
2017-07-26 03:34:21 +0000

    32 Rails: Public Profiles | Assignment Completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def user_has_posts?
+    current_user && current_user.posts.any?
+  end
+  
+  def user_has_comments?
+    current_user && current_user.comments.any?
+  end
 end
